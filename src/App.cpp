8552fab4333eafbd6865b1cafe2338103db88486
adtpdn <adtpdn@gmail.com>
2025-11-09 23:47:58 +0800

    Change the window application

--- src/App.cpp
+++ src/App.cpp
@@ -7,7 +7,7 @@ wxIMPLEMENT_APP(App);
 bool App::OnInit()
 {
     // Create the main application window
-    MainFrame* frame = new MainFrame("C++ Script Combiner", wxPoint(50, 50), wxSize(1200, 800));
+    MainFrame* frame = new MainFrame("github.com/adtpdn/ScriptCombiner", wxPoint(50, 50), wxSize(1200, 800));
     frame->Show(true);
     return true;
 }
